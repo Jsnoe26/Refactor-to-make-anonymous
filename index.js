f5fe1631df37a1cf6f3a4e1905b48e9aa8b0f916
SnoeProductions <>
2021-06-05 20:48:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let reversed = function(str) {
+  return str;
+
+  let lettersArray = str.split('');
+  let reversedLettersArray = lettersArray.reverse();
+  //return reversedLettersArray.join('');
+};
+
+console.log(reversed("LaunchCode"));
